48e7eb3dce8b871554372c2db230943d69a45676
dssilva05 <dssilva.05@gmail.com>
2026-02-18 11:50:38 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'sobral-pinto-v3'; // <--- Sempre que mudar o site, aumente esse número
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting(); // Força o novo SW a assumir o controle imediatamente
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName); // Apaga caches antigos
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim(); // Assume o controle das abas abertas na hora
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
